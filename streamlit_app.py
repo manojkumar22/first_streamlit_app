d71a88512b64db190e4ee7c4645d545e9f620ad9
Manoj Kumar <manoj_kumar22@hotmail.com>
2022-08-10 12:41:45 +0530

    added dataframe to read from amazonaws

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,8 @@ streamlit.text('🥣 🥗 🐔 🥑🍞Teasty & Healthy Fresh Tomato, Onion, Bee
 streamlit.text('🥣 🥗 🐔 🥑🍞Fresh Hot Tea')
 streamlit.text('XYZ yyyy zzzzz aaaaaajdewdewdFresh Hot Tea')
 streamlit.header('🥣 🥗 🐔 🥑🍞 End of Meny🥣 🥗 🐔 🥑🍞')
+
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
