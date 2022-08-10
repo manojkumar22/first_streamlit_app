6bcf70b982105c4e1b3c5bf4be01a4fcd83b1d31
Manoj Kumar <manoj_kumar22@hotmail.com>
2022-08-10 11:25:00 +0530

    More updates to display texts in different format

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+
+streamlit.text('Teasty & Healthy Fresh Tomato, Onion, Beens Upma with Nuts')
+streamlit.text('Fresh Hot Tea')
+streamlit.text('XYZ yyyy zzzzz aaaaaajdewdewdFresh Hot Tea')
